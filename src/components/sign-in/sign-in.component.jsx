9a6ea9bc7a906738406800d6fb3740faf16873a1
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-20 23:09:12 +0100

    Update the Sign In Component to use Firebase auth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -19,15 +19,21 @@ class SignIn extends React.Component {
 
     handleChange = event => {
         const { value, name } = event.target;
-
         this.setState({ [name]: value})
-    }
+    };
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' })
-    }
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' })
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     render() {
         return(
@@ -54,7 +60,7 @@ class SignIn extends React.Component {
                     />
                     <div className='buttons'>
                         <CustomButton type='submit'> Sign In </CustomButton>
-                        <CustomButton onClick={ signInWithGoogle } isGoogleSignIn> Sign In With Google </CustomButton>
+                        <CustomButton type='button' onClick={ signInWithGoogle } isGoogleSignIn> Sign In With Google </CustomButton>
                     </div>
                 </form>
             
