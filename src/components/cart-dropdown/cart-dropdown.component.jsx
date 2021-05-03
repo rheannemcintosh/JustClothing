e0c95559810e238f7c5ab43a08dc16826ebfe939
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-05-03 17:11:56 +0100

    Toggle cart hidden when navigating to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import { withRouter } from 'react-router-dom';
 import CustomButtom from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {cartItems.length ? (
@@ -20,7 +21,12 @@ const CartDropdown = ({ cartItems, history }) => (
                 <span className='empty-message'>Your cart is empty</span>
             )}
         </div>
-        <CustomButtom onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButtom>
+        <CustomButtom onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden());
+        }}>
+            GO TO CHECKOUT
+        </CustomButtom>
     </div>
 );
 
