88764658ce5fa828a05d88ea1492de0ae6e14b39
rheannemcintosh <rheannemcintosh@gmail.com>
2021-05-27 23:12:56 +0100

    Data normalization

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,6 @@
 //import memoize from 'lodash.memoize';
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -20,9 +12,6 @@ export const selectCollection = collectionUrlParam =>
 //export const selectCollection = memoize((collectionUrlParam) =>
     createSelector(
         [selectCollections],
-        collections => 
-            collections.find(
-                collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-            )
-    )
+        collections => collections[collectionUrlParam]
+    );
 //)
